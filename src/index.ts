16c78c269a432e89d8a653ec4419b0a2a04dd95e
Rafael Vieira <rafaelleveske@gmail.com>
2021-01-18 21:30:20 -0300

    Conexão com a api bem sucedida

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ dotenv.config();
 
 const client = new Client();
 
-client.publishMessage({
-  message: 'Hello world',
-  phone: '+5561983379257',
+client.getCredentials({
+  token:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2MTEwMTIwNzEsImV4cCI6MTYxMTA5ODQ3MSwic3ViIjoiNjA1MWRkY2YtODkzMS00YzAxLThhZWItMjQ3OGRiNzVlNTlhIn0.bXEQNm_p4B7qx0JToqvK-V6Vn-LxiUBZBExs3_Kapqs',
 });
--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,45 +1,20 @@
-import AWS from 'aws-sdk';
+import api from '../utils/axios';
 
-interface ISendMessageParams {
-  message: string;
-  phone: string;
+interface AuthState {
+  token: string;
 }
 
 export default class Client {
-  private credentials;
-
-  private sns;
-
-  constructor() {
-    this.credentials = new AWS.Credentials({
-      accessKeyId: String(process.env.AWS_ACCESS_KEY_ID),
-      secretAccessKey: String(process.env.AWS_ACCESS_SECRET),
-    });
-    this.sns = new AWS.SNS({
-      region: process.env.AWS_DEFAULT_REGION,
-      credentials: this.credentials,
-    });
-  }
-
-  public async publishMessage({
-    message,
-    phone,
-  }: ISendMessageParams): Promise<void> {
-    try {
-      const params = {
-        Message: message,
-        PhoneNumber: phone,
-      };
-
-      const sendMessage = await this.sns.publish(params).promise();
-
-      if (sendMessage) {
-        const messageSentId = sendMessage.MessageId;
-
-        console.log(`messageId is ${messageSentId}`);
-      }
-    } catch (err) {
-      console.log(err);
-    }
+  public async getCredentials({ token }: AuthState): Promise<void> {
+    await api
+      .get('profile', {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 }
